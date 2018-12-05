8a321e73e8e9d3219cd105c6c249d1c005f0d710
Luis Bastidas <luisbastir@gmail.com>
2018-12-05 17:23:29 -0600

    setting up vuex state management

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router"
 
 import App from './App.vue'
 import { routes } from "./routes"
+import store from "./store/store"
 
 Vue.use(VueRouter)
 const router = new VueRouter({
@@ -13,5 +14,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
